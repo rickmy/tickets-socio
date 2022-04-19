948434de6df64c967a5ce7432b6acdf07bc8b146
Alejandro <jeffersonaleandro27@gmail.com>
2022-04-19 16:02:01 -0500

    d

--- src/routes/socio.js
+++ src/routes/socio.js
@@ -4,12 +4,55 @@ const { isLoggedIn } = require('../lib/auth');
 const { updateUser } = require('../helpers/loginQuery');
 const pool = require('../database');
 
+const datos = [
+  {
+    fecha: '22-04-25',
+    hora_salida: '09:00',
+    hora_llegada: '09:00',
+    ruta: 'Quito-Loja',
+    asientos: '10',
+    encomiendas: '10',
+  },
+  {
+    fecha: '22-04-25',
+    hora_salida: '09:00',
+    hora_llegada: '09:00',
+    ruta: 'Quito-Guayas',
+    asientos: '10',
+    encomiendas: '10',
+  },
+  {
+    fecha: '22-04-25',
+    hora_salida: '09:00',
+    hora_llegada: '09:00',
+    ruta: 'Quito-Cuenca',
+    asientos: '10',
+    encomiendas: '10',
+  },
+  {
+    fecha: '22-04-25',
+    hora_salida: '09:00',
+    hora_llegada: '09:00',
+    ruta: 'Quito-Guayas',
+    asientos: '10',
+    encomiendas: '10',
+  },
+  {
+    fecha: '22-04-25',
+    hora_salida: '09:00',
+    hora_llegada: '09:00',
+    ruta: 'Quito-Guayas',
+    asientos: '10',
+    encomiendas: '10',
+  },
+];
+
 router.get('/bus', (req, res) => {
   res.render('socio/bus');
 });
 
 router.get('/detalle', (req, res) => {
-  res.render('socio/detalle');
+  res.render('socio/detalle', { datos });
 });
 
 router.get('/bus-coop', (req, res) => {
