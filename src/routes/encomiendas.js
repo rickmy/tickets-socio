06996a3887ad8b88da4ff821ea34b7531c84d4b3
rickmy <ricky081297@hotmail.com>
2022-09-19 17:17:15 -0500

    fix ecomiendas

--- src/routes/encomiendas.js
+++ src/routes/encomiendas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express.Router();
 
 app.get('/encomiendas/list',(req,res)=>{
-  res.json({
+  return res.json({
     code: 202,
     msj:'Hola soy encomienda'
   })
